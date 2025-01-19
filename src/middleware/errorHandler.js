ca8a6d61d6627a4406f7deeab8b8912eb652c5a3
smihallari <smihallari@york.citycollege.eu>
2025-01-19 09:16:27 +0200

    hoping for the best

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,4 @@
 const errorHandler = (err, req, res, next) => {
-    console.error(err.stack); // Log error for debugging
     res.status(err.status || 500).json({
         error: {
             message: err.message || 'Internal Server Error',
